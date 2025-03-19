d803e73940a444c8621df3694c217c7d91c984e0
Lauri <lmt.jaakkola@gmail.com>
2025-03-19 19:04:01 +0200

    Merged the components with App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
+import Greetings from './components/Greetings'
+import NameInput from './components/NameInput'
+import Weekdays from './components/Weekdays';
 
 
 function App() {
 
+  const [name, setName] = useState("");
 
-  return
+
+  return(
+    <div>
+      <Greetings name={name}/>
+      <NameInput onNameChange={setName}/>
+      <Weekdays />
+    </div>
+  )
 }
 
 export default App
